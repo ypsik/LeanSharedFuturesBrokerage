4aa679a520650a5d54fdd53325a99835c67d41a7
aleks <aleks@DESKTOP-SC05CI3>
2026-05-11 22:27:39 +0200

    SharedDataChannelProvider  exported

--- SharedDataChannelProvider.cs
+++ SharedDataChannelProvider.cs
@@ -1,12 +1,15 @@
 ﻿using QuantConnect.Data;
 using QuantConnect.Data.Market;
+using QuantConnect.Interfaces;
 using QuantConnect.Lean.Engine.DataFeeds;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.Text;
 
 namespace SilverQuant.Lean.Brokerages.Futures.Shared
 {
+    [Export(typeof(IDataChannelProvider))]
     public class SharedDataChannelProvider : DataChannelProvider
     {
         public override bool ShouldStreamSubscription(SubscriptionDataConfig config)
