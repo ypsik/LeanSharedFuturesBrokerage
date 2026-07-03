8d369865f6675fc7cb3b7a1218cb11e47ccd47a3
aleks <aleks@DESKTOP-SC05CI3>
2026-07-03 12:09:24 +0200

    implemented GenerateClientId

--- Implementations/OkxFuturesBrokerage.cs
+++ Implementations/OkxFuturesBrokerage.cs
@@ -37,9 +37,6 @@ namespace SilverQuant.Lean.Brokerages.Futures.Implementations
         private readonly SymbolRuleType? _ruleTypeFilter;
         protected override int? FundingRolloverHours => null;
 
-        protected override SharedMarginMode? SharedMarginMode => CryptoExchange.Net.SharedApis.SharedMarginMode.Cross;
-
-
         internal OkxFuturesBrokerage(
             IAlgorithm algorithm,
             OKXRestClient restClient,
