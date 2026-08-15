a71b4e7118a8c494a5ffb00be190ffa04bf77605
aleks <aleks@DESKTOP-SC05CI3>
2026-08-15 19:05:22 +0200

    implemented GetMarginInterestRateModel

--- BrokerageModels/LighterBrokerageModel.cs
+++ BrokerageModels/LighterBrokerageModel.cs
@@ -34,5 +34,34 @@ namespace SilverQuant.Lean.Brokerages.Futures.Shared.BrokerageModels
         {
             return new ImmediateSettlementModel();
         }
+
+        /// <summary>
+        /// Gets the margin interest rate model for Hyperliquid
+        /// </summary>
+        /// <param name="security">The security to get margin interest rate model for</param>
+        /// <returns>The margin interest rate model</returns>
+        /// <remarks>
+        /// CryptoFuture uses a funding rate mechanism for perpetual futures.
+        /// This is handled separately from traditional margin interest.
+        /// </remarks>
+        public override IMarginInterestRateModel GetMarginInterestRateModel(Security security)
+        {
+            // Spot and index trading don't have margin interest
+            if (security.Type == SecurityType.Crypto || security.Type == SecurityType.Index)
+            {
+                return MarginInterestRateModel.Null;
+            }
+
+            // Perpetual futures use funding rates, not traditional margin interest
+            if (security.Type == SecurityType.CryptoFuture &&
+                security.Symbol.ID.Date == SecurityIdentifier.DefaultDate)
+            {
+                // Return the null model which applies no interest
+                // Funding rates are handled separately by the exchange
+                return MarginInterestRateModel.Null;
+            }
+
+            return base.GetMarginInterestRateModel(security);
+        }
     }
 }
